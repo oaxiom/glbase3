1118e119f081140703b35d64b5926326b9d7f167
oaxiom <oaxiom@gmail.com>
2019-04-04 07:43:40 +0800

    renamed bedgraph to flattrack, finally

--- tools/__init__.py
+++ tools/__init__.py
@@ -7,10 +7,10 @@ nothing to see here, move along...
 from .seqToTrk import seqToTrk
 from .wigstep_to_flattrack import wigstep_to_flat
 from .wig_to_flattrack import wig_to_flat
-from .bigwig_to_flattrack import bedgraph_to_flat
+from .bedgraph_to_flattrack import bedgraph_to_flat
 from .gerp_to_flattrack import gerp_to_flat
 from .bed_to_flattrack import bed_to_flat
 from .rnaseq import rnaseqqc
 
-__all__ = ["seqToTrk", "wigstep_to_flat", "gerp_to_flat", "rnaseqqc", 
+__all__ = ["seqToTrk", "wigstep_to_flat", "gerp_to_flat", "rnaseqqc",
     "bedgraph_to_flat", 'bed_to_flat', 'wig_to_flat']
