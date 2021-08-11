6c0ebaa68d83bbc280d340f9420f8e138432e639
oaxiom <oaxiom@gmail.com>
2021-08-11 19:33:53 +0800

    fixed proportional_bar

--- draw.py
+++ draw.py
@@ -2905,7 +2905,7 @@ class draw:
                 for kk in data_dict[k]:
                     if kk not in all_keys:
                         all_keys.append(kk)
-            config.log.info('proportional_bar: Found {} keys'.format(all_keys))
+            print('Found {0} keys'.format(all_keys))
         else:
             all_keys = key_order
 
@@ -2916,6 +2916,7 @@ class draw:
             labs.append(k)
             for kk in all_keys:
                 vals[kk].append(float(data_dict[k][kk]))
+        print(vals)
 
         scaled = {k: [] for k in all_keys}
         sums = None
@@ -2926,16 +2927,15 @@ class draw:
 
         for k in all_keys:
             vals[k] = numpy.array(vals[k])
+            scaled[k] = numpy.array(vals[k])
+            scaled[k] /= sums
+            scaled[k] *= 100
 
-        plot_hei = 0.01*len(labs)
+        plot_hei = (0.8) - (0.04*len(labs))
 
-        # ValueError: bottom cannot be >= top
-
-        if 'figsize' not in kargs: # TODO: Sensible sizes
-            kargs['figsize'] = [4,6]
-
-        fig = self.getfigure(**kargs)
-        fig.subplots_adjust(left=0.35, right=0.95, bottom=0.1, top=0.1+plot_hei)
+        plot.rcParams['pdf.fonttype'] = 42
+        fig = plot.figure(figsize=[4,3])
+        fig.subplots_adjust(left=0.35, right=0.95, bottom=plot_hei,)
         ax = fig.add_subplot(111)
         ax.set_prop_cycle('color', cols)
 
@@ -2945,23 +2945,28 @@ class draw:
 
         bots = numpy.zeros(len(labs))
         for k in vals:
-            ax.barh(ypos, vals[k], 0.7, label=k, left=bots)
-            for y, v, s, b in zip(ypos, vals[k], vals[k], bots):
+            ax.barh(ypos, scaled[k], 0.7, label=k, left=bots)
+            for y, v, s, b in zip(ypos, vals[k], scaled[k], bots):
                 ax.text(b+(s//2), y, '{0:,.0f} ({1:.0f}%)'.format(v, s), ha='center', va='center', fontsize=6)
-            bots += vals[k]
+            bots += scaled[k]
 
         ax.set_yticks(ypos)
         ax.set_yticklabels(labs)
 
+        ax.set_xlim([-2, 102])
+        ax.set_xticks([0, 50, 100])
+        ax.set_xticklabels(['0%', '50%', '100%'])
         ax.set_title(title, size=6)
         ax.legend()
-        plot.legend(loc='upper left')#, bbox_to_anchor=(0.0, -0.4), prop={'size': 6})
-        ax.tick_params(axis='x', labelsize=6)
-        ax.tick_params(axis='y', labelsize=6)
-
+        plot.legend(loc='upper left', bbox_to_anchor=(0.0, -0.4), prop={'size': 6})
+        [t.set_fontsize(6) for t in ax.get_yticklabels()]
+        [t.set_fontsize(6) for t in ax.get_xticklabels()]
+        fig.savefig(filename)
+        fig.savefig(filename.replace('.png', '.pdf'))
+        print('Saved %s' % filename)
         self.do_common_args(ax, **kargs)
         fig.savefig(filename)
 
         real_filename = self.savefigure(fig, filename)
         config.log.info("proportional_bar: Saved '{0}'".format(real_filename))
-        return(real_filename)
+        return real_filename
