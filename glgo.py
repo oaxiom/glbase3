30e6b98b78e846663ca79a754a6b558e07eda2f2
oaxiom <oaxiom@gmail.com>
2023-04-06 11:15:12 +0800

    glgo plots are pretty

--- glgo.py
+++ glgo.py
@@ -47,8 +47,18 @@ class glgo(genelist):
         """
         pass
 
-    def barh(self, filename, ontology=None, maximum=None, pad=1e-105, key="qvalue",
-        label_fontsize=6, mark_significance=True, tight_layout=True, qvalue_limit=0.1, **kargs):
+    def barh(self,
+        filename:str,
+        ontology=None,
+        maximum=None,
+        pad:float = 1e-105,
+        color:str = 'lightgrey',
+        key:str ="qvalue",
+        label_fontsize:int = 6,
+        mark_significance:bool = True,
+        qvalue_limit:float = 0.1,
+        bot_pad:float = 0.1,
+        **kargs):
         """
         **Purpose**
             Draw a barchart for some ontology
@@ -68,6 +78,9 @@ class glgo(genelist):
             qvalue_limit (Optional, default=0.1)
                 The significance limit for this data
 
+            color (Optional, default='lightgrey')
+                color for thew bars
+
             pad (Optional, default=1e-105)
                 By default glgo will -log10 transform the pvalues/qvalues.
                 Occasionally some GO discovery programs will output a p/q-value
@@ -85,14 +98,6 @@ class glgo(genelist):
             mark_significance (Optional, default=True)
                 mark lines of significance at 0.05 and 0.01 p-value/q-value.
 
-            tight_layout (Optional, default=True)
-                use tight_layout to maximise space usage, but the location of the y axis
-                will shift around to make space for the labels and may not be in the same
-                position across multiple graphs.
-
-                If tight_layout is False though then the lables may be truncated on the left hand edge.
-                Use the 'position' argument to modify the position of the graph
-
         **Returns**
             None
         """
@@ -103,7 +108,7 @@ class glgo(genelist):
         else:
             data = self.linearData # take all data
 
-        bar_width = 0.5
+        bar_width = 0.7
         half_bar_width = bar_width / 2.0
 
         if not data: # Check that we got some entries
@@ -131,43 +136,39 @@ class glgo(genelist):
             vals = [-math.log10(i[key]+pad) for i in data] # now padded, etc
 
         if "figsize" not in kargs:
-            kargs["figsize"] = (8, 5)
+            kargs["figsize"] = (7, 6) # Can only accommodate a set number of results;
+
+        mmrelative_hei = 0.1+(0.022*len(data)) # 0.022 is space per item;
 
         fig = self.draw.getfigure(**kargs)
+        fig.subplots_adjust(left=0.82, right=0.98, top=mmrelative_hei, bottom=bot_pad)
         ax = fig.add_subplot(111)
 
+        if "position" in kargs and kargs["position"] and len(kargs["position"]) == 4:
+            ax.set_position(kargs["position"])
+
         x = numpy.arange(0, len(vals))
-        ax.barh(x, vals, bar_width, ec="none", color="grey")
+        ax.barh(x, vals, bar_width, ec="none", color=color)
         ax.set_yticklabels(labs)
-        ax.set_yticks(x+half_bar_width)
+        ax.set_yticks(x) # This is fixed in new matplotlib
         ax.set_frame_on(False)
+
         if ontology:
             ax.set_title(ontology)
-        ax.set_ylim([-half_bar_width, len(vals)-1+bar_width+half_bar_width]) # num_items + bar_width
+        ax.set_ylim([-bar_width, len(vals)-1+bar_width]) # num_items + bar_width
         ax.set_xlim([0, max(vals + [-math.log10(0.05)])])
         if mark_significance:
-            ax.axvline(-math.log10(0.05), ls="-", color="black")
-            ax.axvline(-math.log10(0.01), ls=":", color="grey")
+            #ax.axvline(-math.log10(0.05), ls="-", color="black")
+            ax.axvline(-math.log10(0.01), ls=":", color="red", lw=0.5)
+
         [t.set_fontsize(label_fontsize) for t in ax.get_yticklabels()]
-        [t.set_fontsize(8) for t in ax.get_xticklabels()]
+        [t.set_fontsize(label_fontsize) for t in ax.get_xticklabels()]
+
         ax.tick_params(top=False, bottom=True, left=False, right=False)
-        #ax.axes.get_yaxis().set_visible(False)
-        for line in ax.get_xticklines():
-            line.set_marker(mpllines.TICKDOWN)
-        #[item.set_markeredgewidth(0.0) for item in ax.yaxis.get_ticklines()]
-        #[item.set_markeredgewidth(0.0) for item in ax.xaxis.get_ticklines()]
 
-        # put back x axis:
-        xs = ax.get_xaxis().get_view_interval()
-        ymin = ax.get_yaxis().get_view_interval()[0]
-        ax.add_artist(Line2D(xs, (ymin, ymin), color='black', linewidth=2))
+        ax.tick_params(axis='both', size=6)
+        ax.set_xlabel(key)
 
-        if tight_layout:
-            fig.tight_layout()
-        elif "position" in kargs and kargs["position"] and len(kargs["position"]) == 4:
-            ax.set_position(kargs["position"])
-        else:
-            ax.set_position([0.7, 0.05, 0.25, 0.90])
 
         actual_filename = self.draw.savefigure(fig, filename)
         config.log.info(f"barh(): saved '{actual_filename}'")
