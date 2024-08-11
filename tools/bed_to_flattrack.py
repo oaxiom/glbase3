cceb0d331905a2521c3418cb4629f4229c662ede
oaxiom <oaxiom@gmail.com>
2024-08-11 10:58:46 +0800

    subnuc flats

--- tools/bed_to_flattrack.py
+++ tools/bed_to_flattrack.py
@@ -49,6 +49,54 @@ def is_pe_inner_loop(f, chr_sizes, infilename, gzip, tot_tag_count):
         f.add_chromosome_array(ch.strip(), numpy.array(this_chrom))
         del this_chrom
 
+def is_pe_inner_loop_subnuc(f1, f2, f3, chr_sizes, infilename, gzip, tot_tag_count):
+    # PE version, assumes l and r are frags
+    n = 0
+    for ch in sorted(chr_sizes.keys()):
+        config.log.info(f'Extracting {ch}')
+        this_chrom_subN1 = [0] * (chr_sizes[ch]+1)
+        this_chrom_oneN2 = [0] * (chr_sizes[ch]+1)
+        this_chrom_multN3 = [0] * (chr_sizes[ch]+1)
+
+        for file in infilename:
+            config.log.info(f"Collecting from {file}")
+            oh = open(file, "rt") if not gzip else opengzip.open(file, 'rt')
+            for line in oh: # Skip glbase overhead:
+                if "#" in line:
+                    continue
+
+                line = line.split('\t')
+                if line[0] != ch:
+                    continue
+                #print line
+
+                # We assume a strict BED file
+                l = int(line[1])
+                r = int(line[2])
+                d = r - l
+                
+                if d <= 143:
+                    for bp in range(l, r): this_chrom_subN1[bp] += 1
+                elif d <= 286:
+                    for bp in range(l, r): this_chrom_oneN2[bp] += 1
+                else:
+                    for bp in range(l, r): this_chrom_multN3[bp] += 1                    
+
+                if n % 1000000 == 0 and n>0:
+                    config.log.info("{0:,} tags parsed ({1:.1f}%)".format(n, n/tot_tag_count*100))
+                    
+                n += 1
+                
+            oh.close()
+        #config.log.info('Committing %s' % ch)
+        f1.add_chromosome_array(ch.strip(), numpy.array(this_chrom_subN1))
+        f2.add_chromosome_array(ch.strip(), numpy.array(this_chrom_oneN2))
+        f3.add_chromosome_array(ch.strip(), numpy.array(this_chrom_multN3))
+        
+        del this_chrom_subN1
+        del this_chrom_oneN2
+        del this_chrom_multN3
+
 def is_se_inner_loop(f, chr_sizes, infilename, gzip, read_extend, tot_tag_count):
     # SE version, assumes l and r need read extension based on strand
     n = 0
@@ -89,7 +137,16 @@ def is_se_inner_loop(f, chr_sizes, infilename, gzip, read_extend, tot_tag_count)
         f.add_chromosome_array(ch.strip(), numpy.array(this_chrom))
         del this_chrom
 
-def bed_to_flat(infilename, outfilename, name, isPE, read_extend=None, strand=False, gzip=None, **kargs):
+def bed_to_flat(
+    infilename:str, 
+    outfilename:str, 
+    name:str, 
+    isPE:bool, 
+    read_extend:bool=None, 
+    strand:bool=False, 
+    gzip:bool=None, 
+    sub_nucleosome_tracks:bool =True, 
+    **kargs):
     """
     **Purpose**
         Convert a bed file containing reads into a flat
@@ -134,6 +191,12 @@ def bed_to_flat(infilename, outfilename, name, isPE, read_extend=None, strand=Fa
             If isPE is False then you must specify strand as True and a +/- strand
             must be in your column 6 of your BED (starting at column 1).
 
+        sub_nucleosome_tracks (Optional, default=False)
+            Produce three tracks based on:
+                1. Sub-nucleosomal reads (<=143 bp) filename = ''
+                2. One nucleosomal reads (>143 <286)
+                3. Multi-nucleosomal reads (>=286)
+
         gzip (Optional, default=False)
             The input file(s) is a gzip file.
 
@@ -150,6 +213,9 @@ def bed_to_flat(infilename, outfilename, name, isPE, read_extend=None, strand=Fa
     else:
         assert read_extend is not None, 'You must specify a read_extend if isPE=False'
         assert strand, 'if isPE=False, you must set strand=True and must have strang +/- in column 6 of the BED'
+    if sub_nucleosome_tracks: 
+        assert isPS, 'If sub_nucleosome_tracks=True then isPE must also be True'
+        assert outfilename.endswith('.flat'), 'If sub_nucleosome_tracks=True then we enforce filename conventions with .flat at the end of the filename'
 
     if not isinstance(infilename, list):
         infilename = [infilename]
@@ -157,8 +223,13 @@ def bed_to_flat(infilename, outfilename, name, isPE, read_extend=None, strand=Fa
     n = 0
 
     bin_format = 'i'
-    f = flat_track(filename=outfilename, new=True, name=name, bin_format=bin_format)
-
+    if not sub_nucleosome_tracks:
+        f = flat_track(filename=outfilename, new=True, name=name, bin_format=bin_format)
+    else:
+        f1 = flat_track(filename=outfilename.replace('.flat', 'sub_nuc.flat'), new=True, name=name, bin_format=bin_format)
+        f2 = flat_track(filename=outfilename.replace('.flat', 'one_nuc.flat'), new=True, name=name, bin_format=bin_format)
+        f3 = flat_track(filename=outfilename.replace('.flat', 'mult_nuc.flat'), new=True, name=name, bin_format=bin_format)
+    
     config.log.info("Started %s -> %s" % (infilename, outfilename))
 
     s = time.time()
@@ -194,7 +265,9 @@ def bed_to_flat(infilename, outfilename, name, isPE, read_extend=None, strand=Fa
     for ch in chr_sizes:
         config.log.info('Chromosome: {0} = {1:,} bp'.format(ch, chr_sizes[ch]))
 
-    if isPE:
+    if sub_nucleosome_tracks:
+        is_pe_inner_loop_subnuc(f1, f2, f3, chr_sizes, infilename, gzip, total_read_count)
+    elif isPE:
         is_pe_inner_loop(f, chr_sizes, infilename, gzip, total_read_count)
         f.meta_data['isPE'] = True
     else:
